b397cce2c1d30f8334027c5b27bd37e85c5106c3
roas20 <pqpwwp0220@naver.com>
2024-01-25 18:09:16 +0900

    01.25

--- app.js
+++ app.js
@@ -2,18 +2,35 @@
 const express = require('express');
 const app = express();
 const port = 3330;  // 서버 포트 번호
+const db = require(__dirname + "/public/dbfile/mysql.js");  // db 연동
+const conn = db.init();
 
 
 // app.listen() 서버 실행 함수
-
 app.listen(port, function(){
     console.log('서버연결')
 });
 
+app.use(express.static('public'));
 
 app.get('/',function(req,res){
-    res.sendFile(__dirname + '/public/html/index.html')
+    res.sendFile(__dirname + '/public/html/index.html');
+  
+});
+
+// db 데이터 불러와서 보여줌
+app.get('/a',function(req, res){
+    var sql = "select daytime, theme, name, pnum, agreement from mobileappDB.userinfo_1;";
+    conn.query(sql, function(err, result){
+        if(err){
+            console.log("query is not excuted: " + err);
+        }
+        else{
+            res.send(result);
+        }
+    });
 });
 
 
-app.use(express.static('public'));
+
+
---
+++ public/dbfile/mysql.js
@@ -0,0 +1,22 @@
+var mysql = require("mysql2");
+
+var db_info = {
+    host: 'database-1.c3h8wpy3mw6o.us-east-1.rds.amazonaws.com',
+    port:3306,
+    user: 'admin',
+    password: 'a1234567',
+    database: 'mobileappDB'
+
+};
+
+module.exports = {
+    init: function(){
+        return mysql.createConnection(db_info);
+    },
+    connect: function(conn) {
+        conn.connect(function(err){
+            if(err) console.error("mysql connection error: " + err);
+            else console.log("mysql is connected!");
+        });
+    },
+};
